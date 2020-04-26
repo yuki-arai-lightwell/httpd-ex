a013747f323a0be33fce528d9b7386ccb9051dea
yuki-arai-lightwell <51742667+yuki-arai-lightwell@users.noreply.github.com>
2020-04-26 21:36:55 +0900

    test

---
+++ init.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3.6
+#日本語を処理するのに必要
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+#デバッグ機能を有効にする
+#cgitb.enable()
+
+def html_head():
+  print("Content-type: text/html;\n\n")
+  print("<html><body><h1>test cgi</h1>\n")
+
+def html_end():
+  print("</body></html>\n")
+
---
+++ test.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3.6
+import cgi
+import cgitb
+from http import cookies
+
+#日本語を処理するのに必要
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+#デバッグ機能を有効にする
+cgitb.enable()
+
+#フォーム情報の取り込み
+form = cgi.FieldStorage()
+
+if len(form) == 0:
+    CC=""
+    C=""
+    d=""
+else:
+    CC=form["name"].value
+    event=form["event"].value
+#    print("Set-Cookie: NAME="+CC)
+    C = cookies.SimpleCookie()
+    C["name"] = CC
+    d = form["num"].value
+
+#サンプル
+print("Content-type: text/html;\n\n")
+print("<html><body><h1>test cgi</h1>\n")
+print("<hr>")
+print("席番号:",d)
+print("""
+<form id="form1" name="form1" method="post" action=test.py>
+<br>
+名前：<input type="text" name=name value="名前">
+<input type="submit" value="更新">
+<input type="hidden" name="event" value="mod">
+<input type="hidden" name="num" value="24">
+</form>
+""")
+print("<hr>入力内容<br>")
+#フォームの内容を表示
+print(f'名前：<b>',CC,"</b><br>")
+print(C)
+print("</body></html>\n")
